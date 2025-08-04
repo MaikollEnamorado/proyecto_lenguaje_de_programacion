d58debcbdaff17010f4af569f524ccab6fd84693
Maikoll Enamorado <maikoll.enamorado@unah.hn>
2025-08-04 12:29:36 -0600

    Se creo el widget item_text_button y se modifco la estructura del widget item_sport_field

--- lib/src/widget/item_sport_field.dart
+++ lib/src/widget/item_sport_field.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:reservacion_de_canchas_deportivas/src/widget/item_text_button.dart';
 class ItemSportField extends StatelessWidget {
   const ItemSportField(
     {super.key,
@@ -16,109 +16,90 @@ class ItemSportField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final imagenesValidas = imagenes?.where((img) => img.trim().isNotEmpty).toList() ?? [];
+    double screenwidth = MediaQuery.of(context).size.width;
+    double imageWidth = screenwidth * 0.3;
     return Container(
       margin: EdgeInsets.all(10.0),
       child: Material(
         elevation: 8.0,
         color: Colors.white,
         borderRadius: BorderRadius.circular(10.0),
-        child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 10,),
-              Container(
-                color: Colors.blue,
-                margin: EdgeInsets.all(10.0),
-                child: Text(nombre,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 10,),
+                Text(nombre,
                   style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold,),
                 ),
-              ),
-              SizedBox(height: 5,),
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: Text(tipo,
+                SizedBox(height: 5,),
+                Text(tipo,
                 style: TextStyle(fontSize: 20,),
                 ),
-              ),
-              SizedBox(height: 5,),
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: Text(ubicacion,
+                SizedBox(height: 5,),
+                Text(ubicacion,
                 style: TextStyle(fontSize: 15,),
                 ),
-              ),
-              SizedBox(height: 10,),
-              imagenes != null && imagenes![0].trim().isNotEmpty ? Container(
-                margin: EdgeInsets.all(10.0),
-                child: LayoutBuilder(
-                  builder: (context, constraints) {
-                    double screenwidth = MediaQuery.of(context).size.width;
-                    double imageWidth = screenwidth * 0.3;
-                    return SizedBox(
-                      height: imageWidth,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: imagenes?.length ?? 0,
-                        itemBuilder: (context, index) {
-                          return Row(
-                            children: [
-                              Image.network(
-                                    imagenes![index],
-                                    fit: BoxFit.cover,
-                                    height: imageWidth,
-                                  ),
-                              SizedBox(width: 8.0), // Spacing between images
-                            ],
-                          );
-                        }
-                      ),
-                    );
-                  }
-                ),
-              ) : SizedBox(height: 1,),
-              SizedBox(height: 10,),
-              Divider(),
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: Row(
-                            
-                  children: [
-
-                    Expanded(
-                      child: TextButton.icon(
-                        onPressed: (){},
-                        icon: Icon(Icons.event_available, color: Colors.blue), 
-                        label: Text('Reservar')
-                      ),
+                SizedBox(height: 10,),
+                 imagenesValidas.isNotEmpty ? Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: LayoutBuilder(
+                    builder: (context, constraints) {
+                      return SizedBox(
+                        height: imageWidth,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: imagenes?.length ?? 0,
+                          itemBuilder: (context, index) {
+                            return Row(
+                              children: [
+                                Image.network(
+                                      imagenes![index],
+                                      fit: BoxFit.cover,
+                                      height: imageWidth,
+                                    ),
+                                SizedBox(width: 8.0), // Spacing between images
+                              ],
+                            );
+                          }
+                        ),
+                      );
+                    }
+                  ),
+                ) : SizedBox(height: 1,),
+                SizedBox(height: 10,),
+                Divider(),
+                Row(     
+                  children: [        
+                    ItemTextButton(
+                      label: 'Reservar',
+                      icon: Icons.event_available,
+                      onPressed: () {},
                     ),
                     SizedBox(
                       height: 25,
                       child: VerticalDivider(),
                     ),
-                    Expanded(
-                      child: TextButton.icon(
-                        onPressed: (){}, 
-                        icon: Icon(Icons.schedule, color: Colors.blue),
-                        label: Text('Horarios')
-                      ),
-                    ),
+                    ItemTextButton(
+                      label: 'Horarios', 
+                      icon: Icons.schedule, 
+                      onPressed: () {}),
                     SizedBox(
                       height: 25,
                       child: VerticalDivider()),
-                    Expanded(
-                      child: TextButton.icon(
-                        onPressed: (){}, 
-                        icon: Icon(Icons.history, color: Colors.blue),
-                        label: Text('Historial')
-                      ),
-                    ),
+                    ItemTextButton(
+                      label: 'Historial', 
+                      icon: Icons.history, 
+                      onPressed: () {}),
                   ],
                 ),
-              ),
-            ],
-          ),
+              ],
+            ),
+        ),
       ),
     );
   }
 }
+
