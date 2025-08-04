2e8589184da1dd082b9b328e683c953736530c34
WJAA-UNAH <williamsjavila@gmail.com>
2025-08-04 14:30:15 -0600

    actualizacion del main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:reservacion_de_canchas_deportivas/src/view/home_page.dart';
+import 'package:reservacion_de_canchas_deportivas/src/view/login_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,14 +12,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerConfig: GoRouter(
-        initialLocation: '/home',
+        initialLocation: '/login', // ← inicia en la pantalla de login
         routes: [
           GoRoute(
             path: '/home',
             name: 'home',
             builder: (context, state) => const HomePage(),
-            )
-        ] 
+          ),
+          GoRoute(
+            path: '/login',
+            name: 'login',
+            builder: (context, state) => const LoginPage(),
+          ),
+        ],
       ),
     );
   }
