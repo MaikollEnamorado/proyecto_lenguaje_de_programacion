fc4257b0728c08b830da4c7a677463d8ba8c22b4
Maikoll Enamorado <maikoll.enamorado@unah.hn>
2025-08-09 18:50:20 -0600

    Se cambio el link de las imagenes

--- lib/data/sports_fields_income.dart
+++ lib/data/sports_fields_income.dart
@@ -6,9 +6,9 @@ List<Map<String, dynamic>> sportsFieldsIncome = [
     'tipo': 'Fútbol',
     'ubicacion': 'Rancho Tara frente al intercambiador del norte San Pedro Sula',
     'imagenes':[
-      'https://scontent.fsap15-1.fna.fbcdn.net/v/t39.30808-6/527186494_1281891606662148_1122575975752334899_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeGRszUzcG_1ShSTbhYK967e4ClzOWpcdU_gKXM5alx1TzFo0pbOJecraDI8K4608rFv5yeQNSMnpd7S8fIySOvE&_nc_ohc=2EzKHNQ5ubQQ7kNvwEmzBja&_nc_oc=Adkoo5IoyNQ4fnw8Mzzq06Ic9zok-p-8kdC21eHB5tz7YxPuHqL-K5zBuDN3eUx6czEOT4vMp6qi9qPsPtT5nEqc&_nc_zt=23&_nc_ht=scontent.fsap15-1.fna&_nc_gid=pPScae6DFqfaiJ9QcWvfUg&oh=00_AfVG_ONDFFrDzpQoccIllLu8HLqqxyLIa_r7LmCUauXDWg&oe=6895A114',
-      'https://scontent.fsap15-1.fna.fbcdn.net/v/t39.30808-6/526558856_1281891669995475_3027781846128696542_n.webp?stp=dst-jpg_tt6&_nc_cat=106&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeHYMyG147sGXYUrrdG2MVSxhy4yQ8TndNiHLjJDxOd02N8z2Mrx4-BisiPZ3aj5PiFMkZrXRTS6Vw8CxIm8YzH_&_nc_ohc=5-t6e4v7r4MQ7kNvwHCl4Fx&_nc_oc=Adlh0cd6JO8CNe_UYFUFZ-owdkrgqlAxPra187I1YkDKJWEmtW1_qjd3oOuy7lZeTz_qSqn2M5VkLzsrjqm1JWbx&_nc_zt=23&_nc_ht=scontent.fsap15-1.fna&_nc_gid=7sptLrSR0nP8_krhaGB8gw&oh=00_AfWya6wxPWG4XT7DaHolkPDbMmtZTWNj0KU6rD6-apaTnQ&oe=6895ADCE',
-      'https://scontent.fsap15-1.fna.fbcdn.net/v/t39.30808-6/515038166_1281891673328808_4000028098541576779_n.webp?stp=dst-jpg_tt6&_nc_cat=101&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeGIvrfJKrxUnPgu5GgcYOCYb8LhYiJEQ9FvwuFiIkRD0Sanm-I7JMhZ7DvRIaX_g0Oqx2SranI_lYJbHZpAI5p5&_nc_ohc=nXeHRn1okm8Q7kNvwFMX2B7&_nc_oc=AdnBDMm90n0gY7W43qx2GIEnSqVx8n2ReliiuVDa7kmpzCDdUan6eWtg6LmGmPELMq6VumsGh8l_fo4So_dRnfW6&_nc_zt=23&_nc_ht=scontent.fsap15-1.fna&_nc_gid=zL4QaLz0-OmUZcESdtxORg&oh=00_AfUqqtShMePsydCieXHjsJyoYQYErY2gfetRF5qalc9rHA&oe=6895B681'
+      'https://lh3.googleusercontent.com/gps-cs-s/AC9h4nqJ_99RdXvYVdgNjXUW3p3tDriG1C2Y1qMRpcsA6exrQQgsEGpeMnFfLX1h0Vy79E7rj9ouJHhuEpDt3bE2rwXmt53n6n6eUb9HuNjUt7UlqKxfvefgqVPFs85dgLJSkpxoInq_=s1360-w1360-h1020-rw',
+      'https://lh3.googleusercontent.com/gps-cs-s/AC9h4nrAuq4QbiIuv8Nz1CcECA94GxYxlWTGibzeSQJsBgy73ePpP2NlCx7oUqHFJsqEHQF0Wb_TvFlOvyD4onJiviPG0LmD8_mr3J8pMx8243P_YfE9P0ug_CJFgbpHwiNpp6_LOvN13A=s1360-w1360-h1020-rw',
+      'https://lh3.googleusercontent.com/gps-cs-s/AC9h4npXo3EHaosmjitNyvFr1TOU5mFlJaAIa8MdULuYXly2EPtvv607yx2ghwWGKKKHyusDDtuDHEJzgYu5ehHIgcBPQJcYnz88KADHLT4ztwnHXWR_mkXnnDc9PQEH7KZpG2jt9Hd8=s1360-w1360-h1020-rw'
     ],
     'horarios': [
       'Lunes a Viernes: 8:00 AM - 10:00 PM',
