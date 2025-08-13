98a25d0749e569ef520792554bbb769139d7deb1
Maikoll Enamorado <maikoll.enamorado@unah.hn>
2025-08-13 15:42:55 -0600

    Cambiando la propiedad initialLocation

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
           }
           return null;
         },
-        initialLocation: '/login',
+        initialLocation: '/home',
         routes: [
           GoRoute(
             path: '/home',
